0198d0db17ce5674472034c7d3f91824c1b5909b
Kostiantyn Teltov <dneprokos@gmail.com>
2021-06-16 14:01:20 +0300

    first commit

---
+++ SharpBasicTraining/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SharpBasicTraining
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Variable declaration
+            string message;
+
+            //Assign variable
+            message = " Have a nice day!";
+
+            //Print variable
+            Console.WriteLine("Hello Retail Express Team" + message);
+
+            //Assign new value to variable
+            message = "Starting calculation...";
+            
+            //Print new value
+            Console.WriteLine(message);
+
+            //Calculate a sum from our new method
+            Console.WriteLine(Sum(2, 3));
+
+            //Postpone console close
+            Console.ReadLine();
+        }
+
+        public static int Sum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
