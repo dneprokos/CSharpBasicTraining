1fe66f971f08f0b3a174a0159043bd9181168cf5
Kostiantyn Teltov <dneprokos@gmail.com>
2021-06-23 11:21:33 +0300

    Moving if else switch statement logic example to separate class

---
+++ SharpBasicTraining/1_ConsoleAppAndIfStatements/IfElseSwitchPracticeClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SharpBasicTraining._1_ConsoleAppAndIfStatements
+{
+    public class IfElseSwitchPracticeClass
+    {
+        private const string Question = "Hello! What is you name?";
+        private const string Question2 = "How old are you?";
+        private const string Greeting = "Hello ";
+
+        private static string _answer;
+        private static string _answer2;
+
+        public static void RunLogic()
+        {
+            Console.WriteLine(Question);
+            _answer = Console.ReadLine();
+
+            Console.WriteLine(Question2);
+            _answer2 = Console.ReadLine();
+
+            int age = int.Parse(_answer2);
+
+            switch (age)
+            {
+                case var n when n > 0 && n < 7:
+                    Console.WriteLine(Greeting + _answer + " Sorry path is restricted");
+                    break;
+                case var n when n >= 7 && n <= 12:
+                    Console.WriteLine(Greeting + _answer + " Please go. You have a discount");
+                    break;
+                default:
+                    Console.WriteLine(Greeting + _answer + " Please go and enjoy");
+                    break;
+            }
+
+
+            //if (age > 0 && age < 99)
+            //{
+            //    if (age < 7)
+            //    {
+            //        Console.WriteLine(Greeting + Answer + " Sorry path is restricted");
+            //    }
+            //    else if (int.Parse(Answer2) < 12)
+            //    {
+            //        Console.WriteLine(Greeting + Answer + " Please go. You have a discount");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(Greeting + Answer + " Please go and enjoy");
+            //    }
+            //}
+
+            Console.ReadLine();
+        }
+
+
+        
+    }
+}
--- SharpBasicTraining/Program.cs
+++ SharpBasicTraining/Program.cs
@@ -4,54 +4,9 @@ namespace SharpBasicTraining
 {
     public class Program
     {
-        private const string Question = "Hello! What is you name?";
-        private const string Question2 = "How old are you?";
-        private const string Greating = "Hello ";
-
-        private static string Answer;
-        private static string Answer2;
-
         public static void Main(string[] args)
         {
-            Console.WriteLine(Question);
-            Answer = Console.ReadLine();
             
-            Console.WriteLine(Question2);
-            Answer2 = Console.ReadLine();
-
-            int age = int.Parse(Answer2);
-
-            switch (age)
-            {
-                case 0 - 7:
-                    Console.WriteLine(Greating + Answer + " Sorry path is restricted");
-                    break;
-                case 7 - 12:
-                    Console.WriteLine(Greating + Answer + " Please go. You have a discount");
-                    break;
-                default:
-                    Console.WriteLine(Greating + Answer + " Please go and enjoy");
-                    break;
-            }
-
-
-            //if (age > 0 && age < 99)
-            //{
-            //    if (age < 7)
-            //    {
-            //        Console.WriteLine(Greating + Answer + " Sorry path is restricted");
-            //    }
-            //    else if (int.Parse(Answer2) < 12)
-            //    {
-            //        Console.WriteLine(Greating + Answer + " Please go. You have a discount");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(Greating + Answer + " Please go and enjoy");
-            //    }
-            //}
-            
-            Console.ReadLine();
         }
     }
 }
